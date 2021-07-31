d9e70231cf006feb110dd3c6422ba3f03b134c2a
Victor Miene <viktormiene.vm@gmail.com>
2021-07-31 13:59:24 +0100

    fix : update the primary color in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#effafa", //bg cyan
+        primary: "#5ba4a4",
+        bgCyan: "#effafa", //bg cyan
         cyan: "#eef6f6", //fiter cyan
         darkCyan: "#7b8e8e", //dark cyan
         darkerCyan: "#2c3a3a", //very dark cyan

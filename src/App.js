5154eae16b5f116614143cf2a383c28221e5c226
Victor Miene <viktormiene.vm@gmail.com>
2021-08-07 18:27:19 +0100

    fix: made image in Job component display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import "./App.css";
 import { Job } from "./components/Job";
+import { jobs } from "./data.js";
 
 const details = ["5d ago", "Contract", "USA only"];
 
-const skills = ["Frontend", "Junior", "css", "JavaScript"];
+const skills = [
+  "Frontend",
+  "Junior",
+  "css",
+  "JavaScript",
+  "Junior",
+  "css",
+  "JavaScript",
+];
 
 function App() {
   return (
     <div className="App pt-12 bg-bgCyan">
-      <Job
-        company="MyHome"
-        role="Junior Frontend Developer"
-        details={details}
-        filters={skills}
-      />
-      <Job
-        company="MyHome"
-        role="Junior Frontend Developer"
-        details={details}
-        filters={skills}
-      />
+      {jobs.map((jobItem) => (
+        <Job data={jobItem} />
+      ))}
     </div>
   );
 }
--- src/components/Job.js
+++ src/components/Job.js
@@ -1,41 +1,57 @@
-import React from "react";
-import faceit from "../images/faceit.svg";
 import styled from "styled-components";
-
 const FirstFilterItem = styled.div`
   &:before {
     content: "•";
   }
 `;
 
-export const Job = ({ company, role, filters }) => {
+export const Job = ({ data, children }) => {
+  const {
+    company,
+    logo,
+    when,
+    featured,
+    postion,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools,
+  } = data;
+
+  //const imageName = require('../images/photosnap.svg');
+
+  // const imageName2 = require(str);
   return (
     <div className="w-11/12 mx-auto bg-white mb-12 p-6 border-l-4 border-primary shadow-lg relative font-spartan">
-      <img src={faceit} alt="company logo" className="h-12 absolute -top-6" />
+      <img
+        src={process.env.PUBLIC_URL + logo}
+        alt="company logo"
+        className="h-12 absolute -top-6"
+      />
+
       <div>
         <div className="text-sm text-left mt-6">
           <span className="text-primary font-bold">{company}</span>
+          {when && <span className="text-primary font-bold mx-3">NEW!</span>}
+          {featured && (
+            <span className="text-primary font-bold uppercase">featured</span>
+          )}
         </div>
         <div className="font-bold py-3 text-left">{role}</div>
         <div className="flex flex-wrap text-darkerCyan text-sm">
-          {filters
-            .filter((item, idx) => idx < 3)
-            .map((item, idx) => {
-              if (idx == 0) {
-                return <span className="mr-4">{item}</span>;
-              }
-              return (
-                <FirstFilterItem>
-                  {" "}
-                  <span className="mr-4 ">{item}</span>
-                </FirstFilterItem>
-              );
-            })}
+          <FirstFilterItem>
+            {" "}
+            <span className="mr-4 "></span>
+          </FirstFilterItem>
+          );
         </div>
         <div className="my-4 bg-darkCyan" style={{ height: "1.2px" }} />
       </div>
       <div className="flex flex-wrap content-between h-20">
-        {filters.map((item) => (
+        {tools.map((item) => (
           <span className="px-2 py-1.5 text-primary bg-cyan shadow-sm text-sm font-semibold mr-2">
             {item}{" "}
           </span>

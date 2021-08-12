9cb5a433bd0de30eef32c7804f57f8b9286fc565
Victor Miene <viktormiene.vm@gmail.com>
2021-08-12 22:22:14 +0100

    fix: import AddedFilter into App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { Job } from "./components/Job";
 import { jobs } from "./data.js";
 import AddedFilter from "./components/AddedFilter";
 
-function App() {
+export default function App() {
   const [searchFilters, setSearchFilters] = useState([]);
   const [filter, setFilter] = useState("");
-
   const onFilterChange = (event) => {
     setFilter(event.target.value);
   };
-
   const addFilter = () => {
     setSearchFilters([...searchFilters, filter]);
   };
@@ -19,12 +17,12 @@ function App() {
   const removeFilter = (targetFilter) => {
     setSearchFilters(searchFilters.filter((item) => item !== targetFilter));
   };
-
+  // }
   return (
     <div className="App pt-12 bg-bgCyan">
       <div className="flex flex-wrap w-10/12 mx-auto bg-white px-2 mb-2 py-1">
         {searchFilters.map((filter) => (
-          <AddedFilter removeFilter={removeFilter} name={filter} />
+          <AddedFilter name={filter} />
         ))}
       </div>
       <div>
@@ -48,4 +46,3 @@ function App() {
     </div>
   );
 }
-);

4868ffbf886ebeec0bd1d8487a27da0588c1ef8b
Victor Miene <viktormiene.vm@gmail.com>
2021-08-12 22:10:29 +0100

    feature: create removeFilter function

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import { useState } from "react";
 import "./App.css";
 import { Job } from "./components/Job";
 import { jobs } from "./data.js";
-impoimport { RiCloseLine } from "react-icons/ri";
+import AddedFilter from "./components/AddedFilter";
 
 function App() {
   const [searchFilters, setSearchFilters] = useState([]);
   const [filter, setFilter] = useState("");
+
   const onFilterChange = (event) => {
     setFilter(event.target.value);
   };
+
   const addFilter = () => {
     setSearchFilters([...searchFilters, filter]);
   };
-  // const removeFilter = ( ) => {
 
-  // }
+  const removeFilter = (targetFilter) => {
+    setSearchFilters(searchFilters.filter((item) => item !== targetFilter));
+  };
+
   return (
     <div className="App pt-12 bg-bgCyan">
       <div className="flex flex-wrap w-10/12 mx-auto bg-white px-2 mb-2 py-1">
         {searchFilters.map((filter) => (
-          <AddedFilter name={filter} />
+          <AddedFilter removeFilter={removeFilter} name={filter} />
         ))}
       </div>
       <div>
@@ -44,13 +48,4 @@ function App() {
     </div>
   );
 }
-const AddedFilter = ({ name }) => (
-  <div className="flex items-center mb-2  mr-3 shadow-sm">
-    <span className="text-primary bg-cyan text-xs font-bold py-1 px-3 ">
-      {name}
-    </span>
-    <span className="bg-primary px-0.5 h-full">
-      <RiCloseLine className="text-white h-full" />
-    </span>
-  </div>
 );
